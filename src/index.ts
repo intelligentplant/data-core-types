2eec0900f5049a04c5961c0bcd790bab69ff9cb7
wazzamatazz <grahamcharleswatts@gmail.com>
2018-09-24 13:55:04 +0300

    Add export for TagSearchFilter

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,5 @@ export { EventMessage } from './event-message';
 export { EventMessageProperty } from './event-message-property';
 export { HistoricalTagValues } from './historical-tag-values';
 export { TagDefinition } from './tag-definition';
+export { TagSearchFilter } from './tag-search-filter';
 export { TagValue } from './tag-value';
